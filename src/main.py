8ef54834b30eb805ca15c8d0838b9056a6aaaa4b
zeneto23 <josenilsonpereira000@gmail.com>
2025-07-18 21:43:08 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 import sys
-# DON'T CHANGE THIS !!!
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, send_from_directory
@@ -15,14 +15,12 @@ from src.routes.produto import produto_bp
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
 
-# Configurar CORS para permitir requisições do frontend
 CORS(app)
 
 app.register_blueprint(user_bp, url_prefix='/api')
 app.register_blueprint(categoria_bp, url_prefix='/api')
 app.register_blueprint(produto_bp, url_prefix='/api')
 
-# uncomment if you need to use database
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
